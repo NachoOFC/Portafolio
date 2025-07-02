77ab0c0aaa9a0119a4ce5738e66c565afc99d5e7
Nacho <105531940+NachoOFC@users.noreply.github.com>
2025-07-02 19:27:12 -0400

    ......

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
- nitro: {
-    preset: 'netlify'          // o  'netlify_edge'  si quieres Edge Functions
+  nitro: {
+    preset: "netlify", // o  'netlify_edge'  si quieres Edge Functions
   },
   devtools: { enabled: true },
   css: [
-    'bootstrap/dist/css/bootstrap.min.css',
-    '@fortawesome/fontawesome-free/css/all.min.css',
-    'aos/dist/aos.css'
+    "bootstrap/dist/css/bootstrap.min.css",
+    "@fortawesome/fontawesome-free/css/all.min.css",
+    "aos/dist/aos.css",
   ],
-
-  
-  
-})
+});
